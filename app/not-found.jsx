b7444fe9107b573b839f7721c6a33322f013f817
RachanaKeshaowar <keshrachana0@gmail.com>
2025-08-30 15:21:40 +0530

    Initial commit: add project files

---
+++ app/not-found.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+      <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
+        <h1>Oops, something’s missing—please return to the homepage.</h1>
+      </div>
+    );
+  }
---
+++ app/page.js
@@ -0,0 +1,123 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ArrowRight, BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
+
+import faqs from '@/data/faqs'
+import Link from "next/link";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Card, CardContent } from "@/components/ui/card";
+
+// this is a temp text
+
+const features = [
+    {
+        title: "Intuitive Kanban Boards",
+        description:
+            "Visualze your workflow and optimize team productivity with our easy-to-use kanban boards.",
+        icon: Layout,
+    },
+    {
+        title: "Powerful Sprint Planning",
+        description:
+            "Plan and manage sprints effectively,ensuring your team stays focused on delivering value.",
+        icon: Calendar,
+    },
+    {
+        title: "Comprehensive Reporting",
+        description:
+            "Gaininsight into your team's performance with detailed,customizable reports and analytics.",
+        icon: BarChart,
+    },
+];
+
+export default function Home() {
+    return <div className="min-h-screen">
+        {/* Hero Section*/}
+        <section className="container mx-auto py-20 text-center">
+            <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
+                Streamline Your Workflow  <br />
+                <span className="flex mx-auto gap-3 sm:gap-4 items-center justify-center mb-7">
+                    with{""}
+                    <Image
+                        src={"/scrumLogo.png"}
+                        alt="Logo"
+                        width={400}
+                        height={0}
+                        className="h-14 sm:h-24 w-auto object-contain"
+                    />
+                </span>
+            </h1>
+
+            <p className="text-xl text-grey-300 mb-10 max-w-3xl mx-auto">Taskvault  is a sleek, minimalist website with an intuitive, user-friendly interface focused on seamless
+                navigation and engaging content. It features a responsive design that adapts perfectly across devices,
+                offering a smooth and interactive experience. The site prioritizes speed, security, and personalized content for every user.
+
+            </p>
+            <Link href="/onbording">
+                <Button size="lg" className="mb-10 mr-4">
+                    Get Started <ChevronRight size={18} />
+                </Button>
+
+            </Link>
+            <Link href="#features">
+                <Button size="lg" Varient="outline" className="mr-4">
+                    Learn More <ChevronRight size={18} className="ml-1" />
+                </Button>
+
+            </Link>
+            <section id="features " className="bg-gray-900 py-10 px-5">
+                <div className="container mx-auto">
+                    <h3 className="text-3xl font-bold mb-12 text-center">Key Features </h3>
+                    <div className="grid grid-clos-1 lg:grid-cols-3 gap-8">{features.map((feature, index) => {
+                        return (
+                            <Card key={index} className="bg-gray-800">
+                                <CardContent className='pt6'>
+                                    <div className="flex flex-col items-center">
+                                        <feature.icon className="h-12 w-12 mb-4 mt-5 text-blue-300" /> </div>
+                                    <h4 className="text-xl font-semibold mb-2 "> {feature.title}    </h4>
+                                    <p className="text-gray-300">{feature.description}</p>
+                                </CardContent>
+                            </Card>
+
+                        );
+                    })
+                    }
+                    </div>
+                </div>
+            </section>
+
+            <section className="bg-gray-900 py-20 px-5">
+                <div className="container mx-auto">
+                    <h3 className="text-3xl font-bold mb-5 text-center">
+                        Frequently Asked Question
+                    </h3>
+                    <Accordion type="single" collapsible className="w-full">
+                        {faqs.map((faq, index) => (
+                            <AccordionItem key={index} value={`item-${index}`}>
+                                <AccordionTrigger>{faq.question}</AccordionTrigger>
+                                <AccordionContent>
+                                    {faq.answer}
+                                </AccordionContent>
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
+                </div>
+            </section>
+        </section>
+
+
+        <section className=" py-5 text-center px-5 ">
+            <div className="container mx-auto">
+                <h3 className="text-3xl font-bold mb-5 text-center">
+                    Ready to Transform Your Workflow
+                </h3>
+                <p className="text-xl-mb-12">Join our team for new and better experiment.</p>
+                <Link href='onboarding'>
+                    <Button size='lg' className="mt-7 animate-bounce"> Start For free <ArrowRight className="ml-2 h-5 w-5" />
+                    </Button>
+                </Link>
+            </div>
+        </section>
+    </div>;
+}
+
